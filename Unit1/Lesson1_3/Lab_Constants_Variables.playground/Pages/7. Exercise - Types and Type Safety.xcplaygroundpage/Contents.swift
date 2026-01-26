b2292776cc267ae94ae6f3621ee42e6c7575293e
Joshua Johnson <jjohn021@cougarmail.cccc.edu>
2026-01-25 23:56:38 -0500

    Implement type safety examples for variables
    Added examples demonstrating type safety with variables in Swift.

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,6 +3,9 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking the variable name.
  */
+var firstDecimal: Double = 4.1
+
+var secondDecimal: Double = 4.3
 
 
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
@@ -11,6 +14,11 @@
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 
 
+var trueOrFalse = false
+
+firstDecimal = trueOrFalse
+
+print("It doesnt compile because it doesnt allow to assign boolean to double variable")
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
 
 
