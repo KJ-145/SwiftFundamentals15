6cd5d8d904ad6a71b8486d59d432d9d90af058ed
Heaven48 <Christinme912@gmail.com>
2026-01-26 15:47:25 -0500

    updated
    answer updated

--- Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/1. Exercise - Logical Operators.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_5/Lab_ControlFlow.playground/Pages/1. Exercise - Logical Operators.xcplaygroundpage/Contents.swift
@@ -1,3 +1,4 @@
+
 /*:
 ## Exercise - Logical Operators
 
@@ -35,23 +36,31 @@ print(4 <= 4)
 //:  6. `4 >= 5`
 
 print(false)
+print( 4 >= 5)
+//:  5. `4 <= 4`
+print(true)
+print(4 <= 4)
+
+//:  6. `4 >= 5`
+print(false)
 print(4 >= 5)
 
 //:  7. `(47 > 90) && (47 < 90)`
-print(false) 
+print(false)
 print(true)
-print(47 > 90) && (47 < 90))
+print((47 > 90) && (47 < 90))
 
 //:  8. `(47 > 90) || (47 < 90)`
 print(false)
 print(true)
-print(47 > 90) || (47 < 90))
+print((47 > 90) || (47 < 90))
 
+//:  8. `(47 > 90) || (47 < 90)`
+print(true)
+print((47 > 90) || (47 < 90))
 //:  9. `!true`
 print(false)
 print(!true)
 
 
-/*:
-page 1 of 9  |  [Next: Exercise - If and If-Else Statements](@next)
- */
+
