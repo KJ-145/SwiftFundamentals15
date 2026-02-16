3a28a574d75c5498270fe8fa540175882d62c08a
Heaven48 <Christinme912@gmail.com>
2026-02-15 23:53:08 -0500

    updated

--- Unit2/Lesson2_4/Lab_Structures.playground/Pages/4. App Exercise - Users and Distance.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_4/Lab_Structures.playground/Pages/4. App Exercise - Users and Distance.xcplaygroundpage/Contents.swift
@@ -46,30 +46,15 @@ let walk = Distance(meters: 500)
 print(walk.meters)
 print(walk.feet)
 
-/*:
- In previous app exercises, you've worked with distance in the fitness tracking app example as a simple number. However, distance can be represented using a variety of units of measurement. Create a `Distance` struct that will represent distance in various units of measurement. At a minimum, it should have a `meters` property and a `feet` property. Create a custom initializer corresponding to each property (i.e. if you only have the two properties for meters and feet you will then have two initializers) that will take in a distance in one unit of measurement and assign the correct value to both units of measurements. Hint: *1 meter = 3.28084 feet*.
-
- - Example: If you use the initializer for meters and pass in a distance of 1600, the initializer should set `meters` to 1600 and `feet` to 5249.344.
- */
-
-/*:
- In previous app exercises, you've worked with distance in the fitness tracking app example as a simple number. However, distance can be represented using a variety of units of measurement. Create a `Distance` struct that will represent distance in various units of measurement. At a minimum, it should have a `meters` property and a `feet` property. Create a custom initializer corresponding to each property (i.e. if you only have the two properties for meters and feet you will then have two initializers) that will take in a distance in one unit of measurement and assign the correct value to both units of measurements. Hint: *1 meter = 3.28084 feet*.
-
- - Example: If you use the initializer for meters and pass in a distance of 1600, the initializer should set `meters` to 1600 and `feet` to 5249.344.
- */
-
-/*:
- In previous app exercises, you've worked with distance in the fitness tracking app example as a simple number. However, distance can be represented using a variety of units of measurement. Create a `Distance` struct that will represent distance in various units of measurement. At a minimum, it should have a `meters` property and a `feet` property. Create a custom initializer corresponding to each property (i.e. if you only have the two properties for meters and feet you will then have two initializers) that will take in a distance in one unit of measurement and assign the correct value to both units of measurements. Hint: *1 meter = 3.28084 feet*.
-
- - Example: If you use the initializer for meters and pass in a distance of 1600, the initializer should set `meters` to 1600 and `feet` to 5249.344.
- */
 
 
 //:  Now create an instance of `Distance` called `mile`. Use the initializer for meters to set the distance to 1600. Print out the property for feet and verify that it is equal to 5249.344.
-
+let mile = Distance(meters: 1600)
+print(mile.feet)
 
 //:  Now create another instance of `Distance` and give it some other distance. Ensure that both properties are set correctly.
-
+let inst = Distance(meters: 1788)
+print(inst.feet)
 
 /*:
 [Previous](@previous)  |  page 4 of 10  |  [Next: Exercise - Methods](@next)
--- Unit2/Lesson2_4/Lab_Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_4/Lab_Structures.playground/Pages/5. Exercise - Methods.xcplaygroundpage/Contents.swift
@@ -9,18 +9,26 @@ struct Book {
     var pages: Int
     var price: Double
     
+    func description() {
+        print(" \(title), \(author), \(pages), \(price)")
+    }
 }
 
-
+let describe = Book(title: "", author: "", pages: 0, price: 0.0)
+describe.description()
 //:  A `Post` struct has been created for you below, representing a generic social media post. Add a mutating method on `Post` called `like` that will increment `likes` by one. Then create an instance of `Post` and call `like()` on it. Print out the `likes` property before and after calling the method to see whether or not the value was incremented.
 struct Post {
     var message: String
     var likes: Int
     var numberOfComments: Int
+    
+    mutating func like() {
+        likes += 1
+    }
 
 }
 
-
+var pos
 /*:
 [Previous](@previous)  |  page 5 of 10  |  [Next: App Exercise - Workout Functions](@next)
  */
