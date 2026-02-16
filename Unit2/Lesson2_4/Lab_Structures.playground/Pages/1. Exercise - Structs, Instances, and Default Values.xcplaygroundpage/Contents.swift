b38db6e34b78a1f61a570fae62b0bc5f93995812
Heaven48 <Christinme912@gmail.com>
2026-02-15 22:22:16 -0500

    Update Contents.swift

--- Unit2/Lesson2_4/Lab_Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_4/Lab_Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
@@ -3,19 +3,38 @@
  
  Imagine you are creating an app that will monitor location. Create a `GPS` struct with two variable properties, `latitude` and `longitude`, both with default values of 0.0.
  */
+struct GPS {
+    var longitude: Double = 0.0
+    var latitude: Double = 0.0
 
+}
 
 //:  Create a variable instance of `GPS` called `somePlace`. It should be initialized without supplying any arguments. Print out the latitude and longitude of `somePlace`, which should be 0.0 for both.
 
-
+var somePlace = GPS()
+print(somePlace)
 //:  Change `somePlace`'s latitude to 51.514004, and the longitude to 0.125226, then print the updated values.
-
+somePlace = GPS(longitude: 0.125226, latitude: 51.514004)
+print(somePlace)
 
 //:  Now imagine you are making a social app for sharing your favorite books. Create a `Book` struct with four variable properties: `title`, `author`, `pages`, and `price`. The default values for both `title` and `author` should be an empty string. `pages` should default to 0, and `price` should default to 0.0.
-
+struct Book {
+    var title: String = ""
+    var author: String = ""
+    var pages: Int = 0
+    var price: Int = 0
+}
 
 //:  Create a variable instance of `Book` called `favoriteBook` without supplying any arguments. Print out the title of `favoriteBook`. Does it currently reflect the title of your favorite book? Probably not. Change all four properties of `favoriteBook` to reflect your favorite book. Then, using the properties of `favoriteBook`, print out facts about the book.
+var favouriteBook = Book()
+print(favouriteBook.title)
 
+favouriteBook.title = "The Purpose Driven Life"
+favouriteBook.author = "Rick Warren"
+favouriteBook.pages = 368
+favouriteBook.price = 12
+
+print(" \(favouriteBook.title) book, written by \(favouriteBook.author), has \(favouriteBook.pages) pages and it only costs $\(favouriteBook.price).")
 
 /*:
 page 1 of 10  |  [Next: App Exercise - Workout Tracking](@next)
