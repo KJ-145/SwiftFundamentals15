3ee137e7322d9b753c49df69694f6e78cbf1eb17
Heaven48 <Christinme912@gmail.com>
2026-02-24 01:36:59 -0500

    Update Contents.swift

--- Unit2/Lesson2_5/Lab_Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_5/Lab_Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
@@ -23,18 +23,49 @@ class Spaceship {
         }
     }
 }
+    class Fighter: Spaceship {
+        var weapon: String = ""
+        var remainingFirePower = 5
+        
+        
+        
+        func fire() {
+            if remainingFirePower > 0 {
+                remainingFirePower -= 1
+                print(remainingFirePower)
+            } else {
+                print("You have no more fire power")
+            }
+        }
+    }
+    
+    
+
 //:  Define a new class `Fighter` that inherits from `Spaceship`. Add a variable property `weapon` that defaults to an empty string and a variable property `remainingFirePower` that defaults to 5.
 
-
 //:  Create a new instance of `Fighter` called `destroyer`. A `Fighter` will be able to shoot incoming objects to avoid colliding with them. After initialization, set `weapon` to "Laser" and `remainingFirePower` to 10. Note that since `Fighter` inherits from `Spaceship`, it also has properties for `name`, `health`, and `position`, and has methods for `moveLeft()`, `moveRight()`, and `wasHit()` even though you did not specifically add them to the declaration of `Fighter`. Knowing that, set `name` to "Destroyer," print `position`, then call `moveRight()` and print `position` again.
-
-
+let destroyer = Fighter()
+    
+destroyer.weapon = "laser"
+destroyer.remainingFirePower = 10
+destroyer.name = "Destroyer"
+    
+print(destroyer.position)
+destroyer.moveRight()
+print(destroyer.position)
+destroyer.fire()
 //:  Try to print `weapon` on `falcon`. Why doesn't this work? Provide your answer in a comment or a print statement below, and remove any code you added that doesn't compile.
-
-
+/*let falcon = Spaceship()
+print(falcon.weapon)
+ 
+ this does not compile because weapon is not on spaceship class. Its on fighter */
 //:  Add a method to `fighter` called `fire()`. This should check to see if `remainingFirePower` is greater than 0, and if so, should decrement `remainingFirePower` by one. If `remainingFirePower` is not greater than 0, print "You have no more fire power." Call `fire()` on `destroyer` a few times and print `remainingFirePower` after each method call.
+destroyer.fire()
+destroyer.fire()
+destroyer.fire()
+print(destroyer.remainingFirePower)
 
 
-/*:
-[Previous](@previous)  |  page 2 of 4  |  [Next: Exercise - Override Methods and Properties](@next)
- */
+
+
+ 
